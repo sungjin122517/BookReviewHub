da82c2281a9cc7ed6a949c1eea00dd74193b3950
sungjin122517 <sungjin122517@naver.com>
2024-08-26 17:13:22 +0900

    initial commit

---
+++ src/test/java/org/example/bookreviewhub/BookReviewHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookreviewhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
